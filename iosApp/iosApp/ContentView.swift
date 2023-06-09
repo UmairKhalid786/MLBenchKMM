5c46d6d2096321fe09f2b33bf6b63150c7e888ce
Umair Ali Khalid <umair.khalid786@outlook.com>
2023-06-09 02:11:07 +0200

    iOS project setup complete

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,15 +2,29 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greet()
-
-	var body: some View {
-		Text(greet)
-	}
+    @StateObject private var vm: MoviesViewModel = MoviesViewModel()
+    
+    var body: some View {
+        VStack {
+            if self.vm.appState.status == ResourceStatus.loading {
+                ProgressView()
+            } else if self.vm.appState.status == ResourceStatus.success {
+                List {
+                    ForEach(self.vm.appState.data!.results.filter({ con in
+                        con.title != nil
+                    })) { p in
+                        Text(p.title ?? "")
+                    }
+                }
+            } else {
+                Text("Something went wrong")
+            }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+    static var previews: some View {
+        ContentView()
+    }
 }
---
+++ iosApp/iosApp/MovieViewmodel.swift
@@ -0,0 +1,31 @@
+//
+//  MovieViewmodel.swift
+//  iosApp
+//
+//  Created by Umair Ali Khalid on 09/06/2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import Foundation
+import shared
+
+
+class MoviesViewModel : ObservableObject {
+    
+    @Published var appState = Resource<ContentResponse>(status: ResourceStatus.loading, data: nil, message: nil, error: nil)
+    
+    init() {
+        GetWeatherUseCase().invoke { result, error in
+            if(result != nil) {
+                self.appState = result!
+            }
+        }
+    }
+}
+
+extension ContentResponse.Content: Identifiable {
+    public typealias ID = Int
+    public var id: Int {
+        return hash
+    }
+}
